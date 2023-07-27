2556f4b7ea3ea2da166a8862b47561a5a493c432
Tteum00 <wjdtmddnjs0913@naver.com>
2023-07-27 19:30:06 +0900

    캘린더 생성

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,12 @@
 import Button from "@components/common/Button";
+import Calendar from "@components/common/Calender";
 
 function Home() {
   return (
     <>
       <h1>홈</h1>
       <Button />
+      <Calendar />
     </>
   );
 }
