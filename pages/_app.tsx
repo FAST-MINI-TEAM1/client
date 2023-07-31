d02f2855c4cd06a76283dd24ba51995b779552f4
pildrums <there7788@gmail.com>
2023-07-31 16:00:48 +0900

    chore: :wrench: GlobalStyle 적용 완료

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,12 @@
 import "@styles/globals.scss";
+import GlobalStyle from "@styles/GlobalStyle";
 import type { AppProps } from "next/app";
 import Head from "next/head";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <>
+      <GlobalStyle />
       <Head>
         <title>TEAM1</title>
       </Head>
