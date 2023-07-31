ae5c2071bb44d88102d2a8d146c21738579ccce8
ChoEun-Sang <wh94es@gmail.com>
2023-07-31 15:28:20 +0900

    Feat: :sparkles: 사원 조회 페이지 구현

--- pages/admin/search.tsx
+++ pages/admin/search.tsx
@@ -1,5 +1,6 @@
 import { Select } from "antd";
 import { SearchOutlined } from "@ant-design/icons";
+import ApporvalTabel from "@components/admin/ApprovalTabel";
 
 function SearchPage() {
   const options = [
@@ -12,21 +13,43 @@ function SearchPage() {
       label: "사원번호",
     },
   ];
-  const onChangeHandler = (e) => {
-    e.preventDefault();
-    console.log("onChangeHandler", e.target.value);
-  };
 
   return (
-    <>
-      <div className="searchBox">
+    <section className="search">
+      <div className="searchBar">
         <div className="container">
           <Select defaultValue="1" options={options} />
           <input />
           <SearchOutlined />
         </div>
       </div>
-    </>
+      <div className="info">
+        <h3>기본정보</h3>
+        <div className="container">
+          <ul>
+            <li>
+              <p>사원명</p>
+              <p>홍길동</p>
+            </li>
+            <li>
+              <p>사원번호</p>
+              <p>20230001</p>
+            </li>
+            <li>
+              <p>입사일</p>
+              <p>2023.07.29</p>
+            </li>
+          </ul>
+        </div>
+      </div>
+      <div className="tabel">
+        <h3>연차 / 당직</h3>
+        <div className="details">
+          <ApporvalTabel tableTitle={"결재 대기"} />
+          <ApporvalTabel tableTitle={"결재 완료"} />
+        </div>
+      </div>
+    </section>
   );
 }
 
