da589e0af6bd8aea552ed3e00a8189f42e19eb04
pildrums <there7788@gmail.com>
2023-07-31 15:51:36 +0900

    chore: :wrench: 전역 스타일링 styled-components로 구현

---
+++ styles/theme.ts
@@ -0,0 +1,22 @@
+import { DefaultTheme } from "styled-components";
+
+// 추후 색상 변경
+export const lightTheme: DefaultTheme = {
+  bgColor: "#fff",
+  textColor: "#000",
+  containerBoxColor: "#fff",
+  buttonColor: "#a55eea",
+  borderColor: "#a55eea",
+  buttonTextColor: "#000",
+  hoverColor: "#ccc",
+};
+
+export const darkTheme: DefaultTheme = {
+  bgColor: "#000",
+  textColor: "#fff",
+  containerBoxColor: "#2f2f33",
+  buttonColor: "#a55eea",
+  borderColor: "#a55eea",
+  buttonTextColor: "#fff",
+  hoverColor: "#ccc",
+};
