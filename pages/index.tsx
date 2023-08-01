fbc46c753a5f40a9f71859e8cca3795eb479fa1a
pildrums <there7788@gmail.com>
2023-08-01 17:57:44 +0900

    chore: :wrench: 버튼 테스트

--- pages/index.tsx
+++ pages/index.tsx
@@ -6,7 +6,10 @@ function Home() {
     <>
       <h1>홈</h1>
       <Calendar />
-      <Button acceptButton>승인버튼</Button>
+      <Button acceptButton="true">승인버튼</Button>
+      <Button denyButton="true">반려버튼</Button>
+      <Button empButton="true">사원 로그인 버튼</Button>
+      <Button empButton="true">관리자 로그인 버튼</Button>
     </>
   );
 }
