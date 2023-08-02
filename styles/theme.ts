ccb27c1d108f3860bee1533e35a358fab574e9ba
pildrums <there7788@gmail.com>
2023-08-02 18:21:46 +0900

    chore: :wrench: inputColor, buttonColor 추가

--- styles/theme.ts
+++ styles/theme.ts
@@ -2,14 +2,18 @@ import { DefaultTheme } from "styled-components";
 
 // 추후 색상 변경
 export const theme: DefaultTheme = {
-  bgColor: "#fff",
+  bgColor: "#eee",
+  headerColor: "#e5e5e5",
   textColor: "#000",
   containerBoxColor: "#fff",
   buttonColor: {
     empButton: "#dadada",
-    managerButton: "#f27777",
+    managerButton: "#F27777",
     acceptButton: "#4F75F5",
     denyButton: "#FF6060",
+  },
+  inputColor: {
+    authColor: "#666",
   },
   borderColor: "#a55eea",
   buttonTextColor: "#000",
