473a38443653f5299449ea995202912247de14c6
pildrums <there7788@gmail.com>
2023-08-02 20:55:01 +0900

    init: :tada: 최초 홈 화면 구현 시작

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,15 +1,11 @@
 import Button from "@components/common/Button";
 import Calendar from "@components/common/Calender";
+import Link from "next/link";
 
 function Home() {
   return (
     <>
-      <h1>홈</h1>
-      <Calendar />
-      <Button accept="true">승인버튼</Button>
-      <Button deny="true">반려버튼</Button>
-      <Button employee="true">사원 로그인 버튼</Button>
-      <Button manager="true">관리자 로그인 버튼</Button>
+    
     </>
   );
 }
