9fc4cf6ab690fc0c52770a1e792e00e1381f909d
pildrums <there7788@gmail.com>
2023-08-02 20:47:44 +0900

    init: :tada: 관리자 페이지용 header 1차 완성
    - 세부 페이지 라우팅에 따라서 navigation 링크 스타일링 완성
    - 추후 은상님과 사용대장 페이지 세분화 논의 예정 후 2차 완성 예정

---
+++ components/common/AdminHeader.tsx
@@ -0,0 +1,114 @@
+import Link from "next/link";
+import { useRouter } from "next/router";
+import styled from "styled-components";
+
+function AdminHeader() {
+  const router = useRouter();
+  return (
+    <HeaderBlock>
+      <HeaderContent>
+        <LogoContainer>
+          <Link href="/">
+            <Logo>LOGO</Logo>
+          </Link>
+          <UserWelcome>
+            <span>관리자</span>
+            <span>님, 반갑습니다!</span>
+          </UserWelcome>
+        </LogoContainer>
+          <Nav>
+            <ul>
+              <li>
+                <Link href="/admin">
+                  <a
+                    className={router.pathname === "/admin" ? "active" : ""}
+                  >
+                    요청관리
+                  </a>
+                </Link>
+              </li>
+              <li>
+                <Link href="/admin/search">
+                  <a
+                    className={
+                      router.pathname === "/admin/search" ? "active" : ""
+                    }
+                  >
+                    사원조회
+                  </a>
+                </Link>
+              </li>
+            </ul>
+          </Nav>
+      </HeaderContent>
+    </HeaderBlock>
+  );
+}
+
+const HeaderBlock = styled.header`
+  width: 100%;
+  height: 80px;
+  background: ${(props) => props.theme.headerColor};
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  padding: 0 20px;
+`;
+
+const HeaderContent = styled.div`
+  width: 1320px;
+  height: inherit;
+  /* background: coral; */
+  max-width: 1280px;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+`;
+
+const LogoContainer = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  gap: 1rem;
+`;
+
+const UserWelcome = styled.div`
+  span {
+    &:first-child {
+      color: #00f;
+      font-weight: 600;
+    }
+  }
+`;
+
+const Logo = styled.a`
+  width: 70px;
+  height: 70px;
+  background: #fff;
+  border-radius: 50%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  cursor: pointer;
+  box-shadow: 2px 2px 4px rgba(0, 0, 0, 0.3);
+`;
+
+const Nav = styled.nav`
+  ul {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    gap: 20px;
+    li {
+      a {
+        color: ${(props) => props.theme.inactiveColor};
+        &.active {
+          font-weight: 700;
+          color: ${(props) => props.theme.activeColor};
+        }
+      }
+    }
+  }
+`;
+
+export default AdminHeader;
