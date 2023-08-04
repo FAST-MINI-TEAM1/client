78cd3c77d51e0fa9cb84215afcd26c6642d459bb
pildrums <there7788@gmail.com>
2023-08-04 14:25:08 +0900

    chore: :wrench: axios 인스턴스에 api url, headers 추가

--- lib/api/client.ts
+++ lib/api/client.ts
@@ -1,9 +1,12 @@
 import axios, { AxiosRequestConfig } from "axios";
 
-const BASE_URL = "";
+const BASE_URL = "http://54.79.60.180:8080";
 
 const axiosConfig: AxiosRequestConfig = {
   baseURL: BASE_URL,
+  headers: {
+    "Content-Type": "application/json",
+  },
 };
 
 const client = axios.create(axiosConfig);
