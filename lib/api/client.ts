c330f9dbaef47b3dac70716f3cd3c42ae979d0c0
pildrums <there7788@gmail.com>
2023-08-05 15:50:51 +0900

    chore: :wrench: usetClient 인스턴스 생성

--- lib/api/client.ts
+++ lib/api/client.ts
@@ -1,6 +1,8 @@
 import axios, { AxiosRequestConfig } from "axios";
 
 const BASE_URL = "http://54.79.60.180:8080";
+const accessToken = localStorage.getItem("token");
+const refreshToken = localStorage.getItem("refreshToken");
 
 const axiosConfig: AxiosRequestConfig = {
   baseURL: BASE_URL,
@@ -9,6 +11,16 @@ const axiosConfig: AxiosRequestConfig = {
   },
 };
 
-const client = axios.create(axiosConfig);
+const tokenConfig: AxiosRequestConfig = {
+  baseURL: BASE_URL,
+  headers: {
+    "Content-Type": "application/json",
+    Authorization: `Bearer ${accessToken}`,
+    "Authorization-refresh": `Bearer ${refreshToken}`,
+  },
+};
 
-export default client;
+const client = axios.create(axiosConfig);
+const userClient = axios.create(tokenConfig);
+
+export { client, userClient };
