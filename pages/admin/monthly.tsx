565b0fa903b8dbcaded10364aa8154ff9b5c7107
ChoEun-Sang <wh94es@gmail.com>
2023-08-05 12:54:52 +0900

    refator: :hammer: 불필요한 스타일 삭제

--- pages/admin/monthly.tsx
+++ pages/admin/monthly.tsx
@@ -45,18 +45,6 @@ const Container = styled.section`
   flex-direction: column;
   align-items: center;
   padding: 50px 0 0;
-  .details {
-    display: flex;
-    flex-direction: column;
-    justify-content: center;
-    align-items: center;
-    // width: 700px;
-    padding: 20px 30px 30px;
-    border-radius: 10px;
-    // box-sizing: border-box;
-    // background-color: #fff;
-    // box-shadow: #e2e2e2 0px 5px 10px;
-  }
 `;
 const StyledTabs = styled(Tabs)`
   .ant-tabs-content-holder {
