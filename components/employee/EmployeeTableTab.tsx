cfd8580a889552cd39743c54d34e039dc4f3ec2d
pildrums <there7788@gmail.com>
2023-08-07 21:25:10 +0900

    chore: :wrench: 코드 정렬

--- components/employee/EmployeeTableTab.tsx
+++ components/employee/EmployeeTableTab.tsx
@@ -51,37 +51,35 @@ function EmployeeTableTab() {
   );
 }
 const StyledTabs = styled(Tabs)`
-color: #D9D9D9;
-font-size:16px;
-text-shadow: 0px 3px 7px rgba(81, 81, 81, 0.25);
-h1{
-  color: black;
-  font-size:16px;
-}
-width: 290px;
-.ant-tabs-nav{
-  width: 290px;
- margin: 0 auto;
-}
-.ant-tabs-tab-btn{
-  width: 40px;
-}
-.ant-tabs-nav-wrap{
-
-justify-content: center;
-
-}
-.ant-tabs-ink-bar {
- display: none;
-}
-.ant-tabs-tab:hover {
-  color: black;
-  }
-}
-.ant-tabs .ant-tabs-tab.ant-tabs-tab-active .ant-tabs-tab-btn {
-  color:rgba(31, 191, 146, 1);
-  fontweight: bold;
+  color: #d9d9d9;
+  font-size: 16px;
   text-shadow: 0px 3px 7px rgba(81, 81, 81, 0.25);
-}
+  h1 {
+    color: black;
+    font-size: 16px;
+  }
+  width: 290px;
+  .ant-tabs-nav {
+    width: 290px;
+    margin: 0 auto;
+  }
+  .ant-tabs-tab-btn {
+    width: 40px;
+  }
+  .ant-tabs-nav-wrap {
+    justify-content: center;
+  }
+  .ant-tabs-ink-bar {
+    display: none;
+  }
+  .ant-tabs-tab:hover {
+    color: black;
+  }
+  .ant-tabs .ant-tabs-tab.ant-tabs-tab-active .ant-tabs-tab-btn {
+    color: rgba(31, 191, 146, 1);
+    font-weight: bold;
+    text-shadow: 0px 3px 7px rgba(81, 81, 81, 0.25);
+  }
 `;
+
 export default EmployeeTableTab;
