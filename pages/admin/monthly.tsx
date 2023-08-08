8f238f6cebf42d565121eaf25fdecf30f74a93b9
ChoEun-Sang <wh94es@gmail.com>
2023-08-08 12:05:24 +0900

    chore: :wrench: 불필요한 코드 삭제 및 스타일 수정

--- pages/admin/monthly.tsx
+++ pages/admin/monthly.tsx
@@ -3,7 +3,6 @@ import type { TabsProps } from "antd";
 import MonthlyTable from "@components/admin/MonthlyTable";
 import styled from "styled-components";
 import { useState } from "react";
-import { StringGradients } from "antd/es/progress/progress";
 
 function Monthly() {
   const [tabKey, setTabKey] = useState<string>("");
@@ -49,5 +48,8 @@ const Container = styled.section`
 const StyledTabs = styled(Tabs)`
   &.ant-tabs .ant-tabs-tab.ant-tabs-tab-active .ant-tabs-tab-btn {
     color: coral;
+  }
+  &.ant-tabs .ant-tabs-ink-bar {
+    background: coral;
   }
 `;
