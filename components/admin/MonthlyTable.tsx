897ae9d2b139e73a5ee53b06d8f9c55407379fe4
ChoEun-Sang <wh94es@gmail.com>
2023-08-08 16:28:18 +0900

    chore: :wrench: 한줄 띄어쓰기

--- components/admin/MonthlyTable.tsx
+++ components/admin/MonthlyTable.tsx
@@ -1,8 +1,10 @@
+import { useCallback } from "react";
 import { Table } from "antd";
 import type { ColumnsType } from "antd/es/table";
 import { LeftOutlined, RightOutlined } from "@ant-design/icons";
 import styled from "styled-components";
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import { getMonthlyAnnual, getMonthlyDuty } from "@lib/api/adminAPI";
 
 interface IColumnsData {
   id: number;
@@ -20,6 +22,35 @@ interface IMonthlyTableProps {
 }
 function MonthlyTable({ tabKey }: IMonthlyTableProps) {
   const [year, setYear] = useState(new Date().getFullYear());
+  const [annualData, setAnnualData] = useState([]);
+  const [dutyData, setDutyData] = useState([]);
+
+  useEffect(() => {
+    getMonthlyData();
+  }, []);
+
+  // const getMonthlyData = useCallback(async () => {
+  //   try {
+  //     if (tabKey === "1") {
+  //       const res = await getMonthlyAnnual(year);
+  //       console.log("월별 연차", res);
+  //     } else if (tabKey === "2") {
+  //       const res = await getMonthlyDuty(year);
+  //       console.log("월별 당직", res);
+  //     }
+  //   } catch (error) {
+  //     console.log("월별 조회 실패", error);
+  //   }
+  // }, [tabKey, year]);
+
+  const getMonthlyData = async () => {
+    try {
+      const res = await getMonthlyAnnual(2023);
+      console.log("월별 연차", res);
+    } catch (error) {
+      console.log("월별 조회 실패", error);
+    }
+  };
 
   const columnsData: ColumnsType<IColumnsData> = [
     {
@@ -151,131 +182,131 @@ function MonthlyTable({ tabKey }: IMonthlyTableProps) {
     },
   ];
 
-  const annualData: IColumnsData[] = [
-    {
-      id: 1,
-      empName: "홍길동",
-      empNo: 20230001,
-      month: {
-        jan: 1,
-        feb: 1,
-        mar: 1,
-        apr: 0,
-        may: 1,
-        jun: 0,
-        jul: 1,
-        aug: 1,
-        sep: 3,
-        oct: 1,
-        nov: 1,
-        dec: 3,
-      },
-      total: 15,
-    },
-    {
-      id: 2,
-      empName: "김연아",
-      empNo: 20230002,
-      month: {
-        jan: 3,
-        feb: 2,
-        mar: 1,
-        apr: 0,
-        may: 1,
-        jun: 0,
-        jul: 1,
-        aug: 1,
-        sep: 0,
-        oct: 1,
-        nov: 1,
-        dec: 3,
-      },
-      total: 14,
-    },
-    {
-      id: 3,
-      empName: "박보검",
-      empNo: 20230003,
-      month: {
-        jan: 2,
-        feb: 2,
-        mar: 1,
-        apr: 0,
-        may: 1,
-        jun: 0,
-        jul: 1,
-        aug: 1,
-        sep: 0,
-        oct: 1,
-        nov: 1,
-        dec: 1,
-      },
-      total: 11,
-    },
-  ];
+  // const annualData: IColumnsData[] = [
+  //   {
+  //     id: 1,
+  //     empName: "홍길동",
+  //     empNo: 20230001,
+  //     month: {
+  //       jan: 1,
+  //       feb: 1,
+  //       mar: 1,
+  //       apr: 0,
+  //       may: 1,
+  //       jun: 0,
+  //       jul: 1,
+  //       aug: 1,
+  //       sep: 3,
+  //       oct: 1,
+  //       nov: 1,
+  //       dec: 3,
+  //     },
+  //     total: 15,
+  //   },
+  //   {
+  //     id: 2,
+  //     empName: "김연아",
+  //     empNo: 20230002,
+  //     month: {
+  //       jan: 3,
+  //       feb: 2,
+  //       mar: 1,
+  //       apr: 0,
+  //       may: 1,
+  //       jun: 0,
+  //       jul: 1,
+  //       aug: 1,
+  //       sep: 0,
+  //       oct: 1,
+  //       nov: 1,
+  //       dec: 3,
+  //     },
+  //     total: 14,
+  //   },
+  //   {
+  //     id: 3,
+  //     empName: "박보검",
+  //     empNo: 20230003,
+  //     month: {
+  //       jan: 2,
+  //       feb: 2,
+  //       mar: 1,
+  //       apr: 0,
+  //       may: 1,
+  //       jun: 0,
+  //       jul: 1,
+  //       aug: 1,
+  //       sep: 0,
+  //       oct: 1,
+  //       nov: 1,
+  //       dec: 1,
+  //     },
+  //     total: 11,
+  //   },
+  // ];
 
-  const dutyData: IColumnsData[] = [
-    {
-      id: 1,
-      empName: "정우성",
-      empNo: 20230004,
-      month: {
-        jan: 1,
-        feb: 1,
-        mar: 1,
-        apr: 0,
-        may: 1,
-        jun: 0,
-        jul: 1,
-        aug: 1,
-        sep: 3,
-        oct: 1,
-        nov: 1,
-        dec: 3,
-      },
-      total: 30,
-    },
-    {
-      id: 2,
-      empName: "김혜수",
-      empNo: 20230005,
-      month: {
-        jan: 3,
-        feb: 2,
-        mar: 1,
-        apr: 0,
-        may: 1,
-        jun: 0,
-        jul: 1,
-        aug: 1,
-        sep: 0,
-        oct: 1,
-        nov: 1,
-        dec: 3,
-      },
-      total: 34,
-    },
-    {
-      id: 3,
-      empName: "조인성",
-      empNo: 20230006,
-      month: {
-        jan: 2,
-        feb: 2,
-        mar: 1,
-        apr: 0,
-        may: 1,
-        jun: 0,
-        jul: 1,
-        aug: 1,
-        sep: 0,
-        oct: 1,
-        nov: 1,
-        dec: 1,
-      },
-      total: 31,
-    },
-  ];
+  // const dutyData: IColumnsData[] = [
+  //   {
+  //     id: 1,
+  //     empName: "정우성",
+  //     empNo: 20230004,
+  //     month: {
+  //       jan: 1,
+  //       feb: 1,
+  //       mar: 1,
+  //       apr: 0,
+  //       may: 1,
+  //       jun: 0,
+  //       jul: 1,
+  //       aug: 1,
+  //       sep: 3,
+  //       oct: 1,
+  //       nov: 1,
+  //       dec: 3,
+  //     },
+  //     total: 30,
+  //   },
+  //   {
+  //     id: 2,
+  //     empName: "김혜수",
+  //     empNo: 20230005,
+  //     month: {
+  //       jan: 3,
+  //       feb: 2,
+  //       mar: 1,
+  //       apr: 0,
+  //       may: 1,
+  //       jun: 0,
+  //       jul: 1,
+  //       aug: 1,
+  //       sep: 0,
+  //       oct: 1,
+  //       nov: 1,
+  //       dec: 3,
+  //     },
+  //     total: 34,
+  //   },
+  //   {
+  //     id: 3,
+  //     empName: "조인성",
+  //     empNo: 20230006,
+  //     month: {
+  //       jan: 2,
+  //       feb: 2,
+  //       mar: 1,
+  //       apr: 0,
+  //       may: 1,
+  //       jun: 0,
+  //       jul: 1,
+  //       aug: 1,
+  //       sep: 0,
+  //       oct: 1,
+  //       nov: 1,
+  //       dec: 1,
+  //     },
+  //     total: 31,
+  //   },
+  // ];
 
   return (
     <>
