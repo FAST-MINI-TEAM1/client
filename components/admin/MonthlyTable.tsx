9f6f932b738e0a18d277c5e9fa62af1beacd3fed
ChoEun-Sang <wh94es@gmail.com>
2023-08-09 13:01:30 +0900

    refator: :hammer: 관리자 월별 테이블 컴포넌트 불필요한 코드 삭제 및 수정

--- components/admin/MonthlyTable.tsx
+++ components/admin/MonthlyTable.tsx
@@ -1,10 +1,11 @@
-import { useCallback } from "react";
 import { Table } from "antd";
 import type { ColumnsType } from "antd/es/table";
-import { LeftOutlined, RightOutlined } from "@ant-design/icons";
-import styled from "styled-components";
 import { useEffect, useState } from "react";
 import { getMonthlyAnnual, getMonthlyDuty } from "@lib/api/adminAPI";
+
+interface IMonthlyPros {
+  dataSource: IColumnsData[];
+}
 
 interface IColumnsData {
   id: number;
@@ -17,40 +18,10 @@ interface IMonth {
   [key: string]: number;
 }
 
-interface IMonthlyTableProps {
-  tabKey: string;
-}
-function MonthlyTable({ tabKey }: IMonthlyTableProps) {
-  const [year, setYear] = useState(new Date().getFullYear());
+function MonthlyTable({ dataSource }: IMonthlyPros) {
+  const [year, setYear] = useState(Number(new Date().getFullYear()));
   const [annualData, setAnnualData] = useState([]);
   const [dutyData, setDutyData] = useState([]);
-
-  useEffect(() => {
-    getMonthlyData();
-  }, []);
-
-  // const getMonthlyData = useCallback(async () => {
-  //   try {
-  //     if (tabKey === "1") {
-  //       const res = await getMonthlyAnnual(year);
-  //       console.log("월별 연차", res);
-  //     } else if (tabKey === "2") {
-  //       const res = await getMonthlyDuty(year);
-  //       console.log("월별 당직", res);
-  //     }
-  //   } catch (error) {
-  //     console.log("월별 조회 실패", error);
-  //   }
-  // }, [tabKey, year]);
-
-  const getMonthlyData = async () => {
-    try {
-      const res = await getMonthlyAnnual(2023);
-      console.log("월별 연차", res);
-    } catch (error) {
-      console.log("월별 조회 실패", error);
-    }
-  };
 
   const columnsData: ColumnsType<IColumnsData> = [
     {
@@ -141,11 +112,11 @@ function MonthlyTable({ tabKey }: IMonthlyTableProps) {
     },
     {
       title: "9월",
-      dataIndex: "sep",
+      dataIndex: "sept",
       key: "sep",
       width: 50,
       align: "center",
-      render: (_, data) => <p>{data.month.sep}</p>,
+      render: (_, data) => <p>{data.month.sept}</p>,
     },
     {
       title: "10월",
@@ -182,187 +153,16 @@ function MonthlyTable({ tabKey }: IMonthlyTableProps) {
     },
   ];
 
-  // const annualData: IColumnsData[] = [
-  //   {
-  //     id: 1,
-  //     empName: "홍길동",
-  //     empNo: 20230001,
-  //     month: {
-  //       jan: 1,
-  //       feb: 1,
-  //       mar: 1,
-  //       apr: 0,
-  //       may: 1,
-  //       jun: 0,
-  //       jul: 1,
-  //       aug: 1,
-  //       sep: 3,
-  //       oct: 1,
-  //       nov: 1,
-  //       dec: 3,
-  //     },
-  //     total: 15,
-  //   },
-  //   {
-  //     id: 2,
-  //     empName: "김연아",
-  //     empNo: 20230002,
-  //     month: {
-  //       jan: 3,
-  //       feb: 2,
-  //       mar: 1,
-  //       apr: 0,
-  //       may: 1,
-  //       jun: 0,
-  //       jul: 1,
-  //       aug: 1,
-  //       sep: 0,
-  //       oct: 1,
-  //       nov: 1,
-  //       dec: 3,
-  //     },
-  //     total: 14,
-  //   },
-  //   {
-  //     id: 3,
-  //     empName: "박보검",
-  //     empNo: 20230003,
-  //     month: {
-  //       jan: 2,
-  //       feb: 2,
-  //       mar: 1,
-  //       apr: 0,
-  //       may: 1,
-  //       jun: 0,
-  //       jul: 1,
-  //       aug: 1,
-  //       sep: 0,
-  //       oct: 1,
-  //       nov: 1,
-  //       dec: 1,
-  //     },
-  //     total: 11,
-  //   },
-  // ];
-
-  // const dutyData: IColumnsData[] = [
-  //   {
-  //     id: 1,
-  //     empName: "정우성",
-  //     empNo: 20230004,
-  //     month: {
-  //       jan: 1,
-  //       feb: 1,
-  //       mar: 1,
-  //       apr: 0,
-  //       may: 1,
-  //       jun: 0,
-  //       jul: 1,
-  //       aug: 1,
-  //       sep: 3,
-  //       oct: 1,
-  //       nov: 1,
-  //       dec: 3,
-  //     },
-  //     total: 30,
-  //   },
-  //   {
-  //     id: 2,
-  //     empName: "김혜수",
-  //     empNo: 20230005,
-  //     month: {
-  //       jan: 3,
-  //       feb: 2,
-  //       mar: 1,
-  //       apr: 0,
-  //       may: 1,
-  //       jun: 0,
-  //       jul: 1,
-  //       aug: 1,
-  //       sep: 0,
-  //       oct: 1,
-  //       nov: 1,
-  //       dec: 3,
-  //     },
-  //     total: 34,
-  //   },
-  //   {
-  //     id: 3,
-  //     empName: "조인성",
-  //     empNo: 20230006,
-  //     month: {
-  //       jan: 2,
-  //       feb: 2,
-  //       mar: 1,
-  //       apr: 0,
-  //       may: 1,
-  //       jun: 0,
-  //       jul: 1,
-  //       aug: 1,
-  //       sep: 0,
-  //       oct: 1,
-  //       nov: 1,
-  //       dec: 1,
-  //     },
-  //     total: 31,
-  //   },
-  // ];
-
   return (
     <>
-      <Container>
-        <YearChanger>
-          <Arrow type="button" onClick={() => setYear(year - 1)}>
-            <LeftOutlined />
-          </Arrow>
-          <Year>{year}년</Year>
-          <Arrow type="button" onClick={() => setYear(year + 1)}>
-            <RightOutlined />
-          </Arrow>
-        </YearChanger>
-        <Table
-          columns={columnsData}
-          dataSource={tabKey === "1" ? annualData : dutyData}
-          size="small"
-          bordered
-        />
-      </Container>
+      <Table
+        columns={columnsData}
+        dataSource={dataSource}
+        size="small"
+        bordered
+      />
     </>
   );
 }
 
 export default MonthlyTable;
-
-const Container = styled.div`
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  padding: 20px 30px 30px;
-  border-radius: 10px;
-  box-sizing: border-box;
-  background-color: #fff;
-  box-shadow: #e2e2e2 0px 5px 10px;
-`;
-
-const YearChanger = styled.div`
-  display: flex;
-  align-items: center;
-  gap: 1rem;
-  margin-bottom: 20px;
-`;
-
-const Year = styled.span`
-  font-size: 30px;
-`;
-const Arrow = styled.button`
-  color: #333;
-  border: 0;
-  font-size: 1rem;
-  background-color: transparent;
-  cursor: pointer;
-  transition: all 0.2s ease-in-out;
-  &:hover {
-    transform: scale(1.2);
-    color: #1ebf91;
-  }
-`;
