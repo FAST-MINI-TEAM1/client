1664e229c4901c8eb4553c80e77be7c577268280
pildrums <there7788@gmail.com>
2023-08-09 19:02:42 +0900

    chore: :wrench: accessToken 수정

--- lib/api/client.ts
+++ lib/api/client.ts
@@ -2,9 +2,7 @@ import axios, { AxiosRequestConfig } from "axios";
 
 const BASE_URL = "http://54.79.60.180:8080";
 const accessToken =
-  typeof window !== "undefined" && localStorage.getItem("token");
-const refreshToken =
-  typeof window !== "undefined" && localStorage.getItem("refreshToken");
+  typeof window !== "undefined" && localStorage.getItem("Token");
 console.log(accessToken);
 const axiosConfig: AxiosRequestConfig = {
   baseURL: BASE_URL,
@@ -17,8 +15,7 @@ const tokenConfig: AxiosRequestConfig = {
   baseURL: BASE_URL,
   headers: {
     "content-type": "application/json",
-    Authorization: `Bearer ${accessToken}`,
-    "Authorization-refresh": `Bearer ${refreshToken}`,
+    Authorization: `${accessToken}`,
   },
 };
 
