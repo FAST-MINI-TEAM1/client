0d1711f0a84c1862aa5b00d4540675252cad8a32
pildrums <there7788@gmail.com>
2023-08-11 23:34:16 +0900

    fix: :bug: accessToken 문제 해결

--- lib/api/client.ts
+++ lib/api/client.ts
@@ -4,7 +4,7 @@ const BASE_URL = "http://3.34.110.127";
 
 // const BASE_URL = "https://alsl1wpqkf1.shop";
 
-const accessToken =
+export const accessToken =
   typeof window !== "undefined" && localStorage.getItem("Token");
 console.log(accessToken);
 const axiosConfig: AxiosRequestConfig = {
@@ -18,7 +18,7 @@ const tokenConfig: AxiosRequestConfig = {
   baseURL: BASE_URL,
   headers: {
     "content-type": "application/json",
-    Authorization: `${accessToken}`,
+    Authorization: accessToken,
   },
 };
 
@@ -26,8 +26,7 @@ const adminConfig: AxiosRequestConfig = {
   baseURL: BASE_URL,
   headers: {
     "content-type": "application/json",
-    Authorization:
-      "eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJhZG1pbkBnbWFpbC5jb20iLCJpYXQiOjE2OTE2NjQ0NTYsImV4cCI6MTY5MTc1MDg1Nn0.fRiZUByNNC5R4XPL1FrOAvH5RB6PBkIOMUR9ks-Sne4",
+    Authorization: accessToken,
   },
 };
 
