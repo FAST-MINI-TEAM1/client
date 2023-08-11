25da908339e99c43ef52485238a3654432ba958b
ChoEun-Sang <wh94es@gmail.com>
2023-08-11 14:26:02 +0900

    chore::wrench:배포용용URL 주석

--- lib/api/client.ts
+++ lib/api/client.ts
@@ -1,9 +1,8 @@
 import axios, { AxiosRequestConfig } from "axios";
 
-
 const BASE_URL = "http://3.34.110.127";
 
-const BASE_URL = "https://alsl1wpqkf1.shop";
+// const BASE_URL = "https://alsl1wpqkf1.shop";
 
 const accessToken =
   typeof window !== "undefined" && localStorage.getItem("Token");
